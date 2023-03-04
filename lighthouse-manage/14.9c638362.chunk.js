920cebaa360603948a0cfca4483524b6c5c034a1
王湛 <wangzhan@hnzycfc.com>
2023-03-04 13:09:57 +0800

    fix: 修改页面

--- lighthouse-manage/14.9c638362.chunk.js
+++
@@ -1,1 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[14],{457:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return i}));var a=t(65),c=t(66),r=t(0),u=t.n(r),s=t(429),i=function(){var e=Object(s.e)();return Object(r.useEffect)((function(){c.a.getSession(a.a.AUTHORIZATION)||e.replace("/login")}),[e]),u.a.createElement(u.a.Fragment,null)}}}]);
