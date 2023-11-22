b6b7b824fae8e32073f2592b435e73a8a4188d64
wangzhan <abcwangzhan@163.com>
2023-11-22 23:14:03 +0800

    feat: reborn测试

---
+++ reborn/precache-manifest.75ce29a856005de4742c88a08477de1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62d3dbee788f08d7d4d8",
+    "url": "./static/css/main.57ea1c7f.chunk.css"
+  },
+  {
+    "revision": "62d3dbee788f08d7d4d8",
+    "url": "./static/js/main.62d3dbee.chunk.js"
+  },
+  {
+    "revision": "047a30160a97910182cc",
+    "url": "./static/js/1.047a3016.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2a8d0af8e0a63761ae386a06776acfae",
+    "url": "./index.html"
+  }
+];
